ff3501fdb75656742e46e80c61de57cf2887210b
Daniel Jacques <djacques633@gmail.com>
2020-08-10 23:48:34 -0400

    Replace css with styled components

--- src/components/Particles/Particles.jsx
+++ src/components/Particles/Particles.jsx
@@ -1,17 +1,15 @@
 import React, {Component} from 'react';
 import config from "./index.json";
 import Particles from 'react-particles-js';
-import s from "./Particles.css";
-import './Particles.css'
+import {Canvas, Container} from './Particles.style';
+
 export default class ParticlesCom extends Component {
 
     render() {
-        console.log(s);
         return(
-            // <h1>hello world</h1>
-            <div className = "container">
-                <Particles className = "canvas" params = {config}></Particles>
-            </div>
+            <Container>
+                <Canvas params = {config}></Canvas>
+            </Container>
         )
     }
 
